751254f3155db01262a84b5e67969c0a5c4dda15
adam grocholski <adgroc@outlook.com>
2017-02-16 10:48:42 -0600

    added query context

---
+++ ClientLibrary/Microsoft.ProjectOxford.Search/Microsoft.ProjectOxford.Search.Autosuggest/QueryContext.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Search.Autosuggest
+{
+    public class QueryContext
+    {
+        #region Properties
+
+        [JsonProperty("originalQuery")]
+        public string OriginalQuery
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
