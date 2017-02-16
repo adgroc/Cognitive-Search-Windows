85dfd68fa8649ae902d6ef8cd92635ecfb0540ea
adam grocholski <adgroc@outlook.com>
2017-02-16 10:56:03 -0600

    added autosuggest request

---
+++ ClientLibrary/Microsoft.ProjectOxford.Search/Microsoft.ProjectOxford.Search.Autosuggest/AutosuggestRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Search.Autosuggest
+{
+    public class AutosuggestRequest
+    {
+        #region Properties
+
+        public string Query
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
