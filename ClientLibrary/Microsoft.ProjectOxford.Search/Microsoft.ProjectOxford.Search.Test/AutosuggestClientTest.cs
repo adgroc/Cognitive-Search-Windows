d0353310b6182d61b25d7e1bf8df126f658fa384
adam grocholski <adgroc@outlook.com>
2017-02-16 12:17:09 -0600

    initial unit test for autosuggest

---
+++ ClientLibrary/Microsoft.ProjectOxford.Search/Microsoft.ProjectOxford.Search.Test/AutosuggestClientTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.ProjectOxford.Search.Autosuggest;
+
+namespace Microsoft.ProjectOxford.Search.Test
+{
+    [TestClass]
+    public class AutosuggestClientTest : SearchTest
+    {
+        #region Test Methods
+
+        [TestMethod]
+        [TestCategory("Autosuggest")]
+        [ExpectedException(typeof(QueryNotSpecifiedException))]
+        public void ValidateTest_QueryNotSpecified()
+        {
+            var request = new AutosuggestRequest();
+            request.Validate();
+        }
+
+        [TestMethod]
+        [TestCategory("Autosuggest")]
+        public void GetSuggestionsTest()
+        {
+            var request = new AutosuggestRequest();
+            request.Query = "bill gates";
+            request.Market = "en-us";
+
+            var client = new AutosuggestClient(this.apiKey);
+
+            var response = client.GetSuggestions(request);
+
+            Assert.Inconclusive();
+        }
+
+        #endregion Test Methods
+    }
+}
---
+++ ClientLibrary/Microsoft.ProjectOxford.Search/Microsoft.ProjectOxford.Search.Test/SearchTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+
+namespace Microsoft.ProjectOxford.Search.Test
+{
+    [TestClass]
+    public abstract class SearchTest
+    {
+        #region Fields
+
+        protected string apiKey = "";
+
+        #endregion Fields
+
+        #region Test Initialization
+
+        /// <summary>
+        /// Intializes this instance.
+        /// </summary>
+        [TestInitialize]
+        public void Intialize()
+        {
+            apiKey = ConfigurationManager.AppSettings["apiKey"];
+        }
+
+        #endregion Test Initialization
+    }
+}
