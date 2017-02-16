feb105cbb56063ec58f91a4dfab51c911ec394b8
adam grocholski <adgroc@outlook.com>
2017-02-16 16:53:48 -0600

    video search response

---
+++ ClientLibrary/Microsoft.ProjectOxford.Search/Video/Instrumentation.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Search.Video
+{
+    /// <summary>
+    /// Instrumentation returned from video search API.
+    /// </summary>
+    public class Instrumentation
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the page load ping URL.
+        /// </summary>
+        /// <value>
+        /// The page load ping URL.
+        /// </value>
+        [JsonProperty("pageLoadPingUrl")]
+        public string PageLoadPingUrl
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
