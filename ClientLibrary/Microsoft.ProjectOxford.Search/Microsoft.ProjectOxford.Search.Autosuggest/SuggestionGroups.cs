db088e35478ea1cab3d1cef8f169c90062bccc8c
adam grocholski <adgroc@outlook.com>
2017-02-16 10:47:10 -0600

    added suggestion groups

---
+++ ClientLibrary/Microsoft.ProjectOxford.Search/Microsoft.ProjectOxford.Search.Autosuggest/SuggestionGroups.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Search.Autosuggest
+{
+    public class SuggestionGroups
+    {
+        #region Constructors
+
+        public SuggestionGroups()
+        {
+            this.SearchSuggestions = new List<SearchSuggestion>();
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        [JsonProperty("name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("searchSuggestions")]
+        public List<SearchSuggestion> SearchSuggestions
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
