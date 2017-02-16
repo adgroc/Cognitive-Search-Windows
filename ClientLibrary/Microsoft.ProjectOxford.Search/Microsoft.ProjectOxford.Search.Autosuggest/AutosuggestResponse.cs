e27145eafcd838d0db13a5aee6ea48a16a95025c
adam grocholski <adgroc@outlook.com>
2017-02-16 10:54:04 -0600

    added autosuggest response

---
+++ ClientLibrary/Microsoft.ProjectOxford.Search/Microsoft.ProjectOxford.Search.Autosuggest/AutosuggestResponse.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Search.Autosuggest
+{
+    public class AutosuggestResponse
+    {
+        #region Constructors
+
+        public AutosuggestResponse()
+        {
+            this.Instrumentation = new Instrumentation();
+            this.QueryContext = new QueryContext();
+            this.SuggestionGroups = new SuggestionGroups();
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        [JsonProperty("_type")]
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("instrumentation")]
+        public Instrumentation Instrumentation
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("queryConext")]
+        public QueryContext QueryContext
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("suggestionGroups")]
+        public SuggestionGroups SuggestionGroups
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
