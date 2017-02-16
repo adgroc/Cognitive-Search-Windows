84c6aa204541d41c405a767743d1efd21344aa8e
adam grocholski <adgroc@outlook.com>
2017-02-16 10:44:17 -0600

    search suggestion class

---
+++ ClientLibrary/Microsoft.ProjectOxford.Search/Microsoft.ProjectOxford.Search.Autosuggest/SearchSuggestion.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Search.Autosuggest
+{
+    public class SearchSuggestion
+    {
+        #region Properties
+
+        [JsonProperty("url")]
+        public string Url
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("urlPingSuffix")]
+        public string UrlPingSuffix
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("displayText")]
+        public string DisplayText
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("query")]
+        public string Query
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("documents")]
+        public string SearchKind
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
