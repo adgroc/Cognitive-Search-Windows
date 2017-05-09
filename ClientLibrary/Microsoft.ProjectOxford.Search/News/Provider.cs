07bfd24ad605e4b933d2d48fdcabd4ffb0aae11a
Adam Grocholski <adgroc@outlook.com>
2017-05-09 16:08:16 -0500

    objs for news search

---
+++ ClientLibrary/Microsoft.ProjectOxford.Search/News/Provider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ProjectOxford.Search.News
+{
+    /// <summary>
+    /// News provider.
+    /// </summary>
+    public class Provider
+    {
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
